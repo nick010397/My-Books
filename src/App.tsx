9c0619f9ee65abf94499809ea8eab82ca8875ea9
nick010397 <01037231774@arreo.com>
2022-12-19 23:58:47 +0900

    Router설정완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Add from "./Pages/Add";
+import Detail from "./Pages/Detail";
+import Edit from "./Pages/Edit";
+import Error from "./Pages/Error";
+import Home from "./Pages/Home";
+import NotFound from "./Pages/NotFound";
+import Signin from "./Pages/Signin";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ErrorBoundary FallbackComponent={Error}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/edit/:id" component={Edit} />
+          <Route exact path="/book/:id" component={Detail} />
+          <Route exact path="/add" component={Add} />
+          <Route exact path="/signin" component={Signin} />
+          <Route exact path="/" component={Home} />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 }
 
